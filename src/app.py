a20a7ce63288d9268cc04939c0657bd0b92c99cb
eugenemuthenya-debug <eugenemuthenya@gmail.com>
2026-07-24 20:26:19 +0300

    installed npm tools

--- src/app.py
+++ src/app.py
@@ -1,12 +1,12 @@
 from flask_cors import CORS
 from flask import Flask, request , jsonify
-from flask_bcrypt import Bcrypt
-from flask_jwt_extended import (
-    JWTManager,
-    create_access_token,
-    jwt_required,
-    get_jwt_required,
-)
+# from flask_bcrypt import Bcrypt
+# from flask_jwt_extended import (
+#     JWTManager,
+#     create_access_token,
+#     jwt_required,
+#     get_jwt_required,
+# )
 import pymysql
 import os
 
