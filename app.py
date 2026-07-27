1e8e984a9a1a952f3459d7f48e0bcafa9d4a5a6c
eugenemuthenya-debug <eugenemuthenya@gmail.com>
2026-07-27 16:16:13 +0300

    enabled access_tokens

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask_cors import CORS
 from flask_bcrypt import Bcrypt
 from flask import Flask, request , jsonify
 # from flask_bcrypt import Bcrypt
-# from flask_jwt_extended import (
-#     JWTManager,
-#     create_access_token,
-#     jwt_required,
-#     get_jwt_required,
-# )
+from flask_jwt_extended import (
+    JWTManager,
+    create_access_token,
+    jwt_required,
+    get_jwt_required,
+)
 import psycopg
 from psycopg.rows import dict_row
 # since we are already using postgresql we use this psycopg library.
@@ -19,6 +19,13 @@ app = Flask(__name__)
 CORS(app)
 
 bcrypt=Bcrypt(app)
+
+# OUR CONFIGS
+# app.config:app is an object and has libraries in it such as config and we can store our data in there. In our case we are storing jwt_secret_key, this is a key which will be assigned a value
+# os.environ:python talks to our os to get environment variables and for us it is in render.
+# .get():we use the function to prevent crashing of the app when we want to get the value rather than the key , value pair itself.
+# We never code this in our code for security purposes.
+app.config["JWT_SECRET_KEY"]=os.environ.get("JWT_SECRET_KEY")
 
 # we have made two changes, the database name variable is called dbname 
 # and we require the port 
@@ -103,10 +110,14 @@ def signup():
         connection.close()
 
 
-#----------Log in-------------(Landlord)
+#----------Log in-------------(Landlord)[Success]
 @app.route("/api/login", methods=["POST"])
 def signin():
     data = request.get_json()
+
+   
+
+
 
     landlord_email= data.get("landlord_email", "").strip()
     landlord_password = data.get("landlord_password", "").strip()
@@ -123,19 +134,23 @@ def signin():
 
         if not landlord_user or not bcrypt.check_password_hash(landlord_user["password_hash"],landlord_password):
             return jsonify({"error":"Invalid credentials"}),401
+
+        # we wanna create our access token but they will be uniquely identified via landlord_id
+        access_token = create_access_token(identity=str(landlord_user["landlord_id"]))
         return jsonify({
             "message":"Login successful",
             "landlord_user":{
                 "landlord_id":landlord_user["landlord_id"],
                 "landlord_name": landlord_user["landlord_name"],
                 "landlord_email":landlord_user["landlord_email"],
-                "phone_number":landlord_user["phone_number"]
+                "phone_number":landlord_user["phone_number"],
+                "access_token":access_token
             }
         }),200
     except Exception as e:
         traceback.print_exc()
-        return jsonify({"error":str(e)}),500
-        # return jsonify({"error":"Something went wrong. Please try again."}),500
+        # return jsonify({"error":str(e)}),500
+        return jsonify({"error":"Something went wrong. Please try again."}),500
     finally:
         cursor.close()
         connection.close()
